fd6d3f456a89e21b425d597b9224b72f4f917f50
Julien Levieux <julien.levieux@drotek.com>
2023-02-03 14:35:59 +0100

    404 pages is done

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -2,17 +2,26 @@
 /* eslint-disable */
 import { useRouteError } from "react-router-dom";
 
+//styles
+import './styles.scss'
+
+// Components
+import Header from "../../Components/Header";
+import Footer from "../../Components/Footer";
+
+
 const NotFound = () => {
     const error = useRouteError();
     console.error(error);
   
     return (
       <div id="error-page">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
+        <Header />
+        <div className="error">
+            <h1 className="error__status">404</h1>
+            <p className="error__content">Oups! La page que vous demandez n'éxiste pas</p>
+            <a  className="error__link" href="/">Retournez sur la page d'acceuil</a>
+        </div>
       </div>
     );
 }
