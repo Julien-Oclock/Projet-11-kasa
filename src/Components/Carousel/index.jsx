ff1453610954ea908124d231ff724d6166ae0d7f
Julien Levieux <julien.levieux@drotek.com>
2023-03-16 11:57:14 +0100

    project is done

--- src/Components/Carousel/index.jsx
+++ src/Components/Carousel/index.jsx
@@ -15,16 +15,23 @@ const Carousel = ({ Images }) => {
                 updateCurrentIndex(
                   currentIndex === 0 ? Images.length - 1 : currentIndex - 1
                 )
-              }>{'<'}
-        </div>
+              }>{
+                <svg className='arrow_left' width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/>
+                </svg>
+                }
+            </div>
             <div
               className="nav__arrow_right"
               onClick={() =>
                 updateCurrentIndex(
                   currentIndex === Images.length - 1 ? 0 : currentIndex + 1
                 )
-              }
-            >&gt;</div>
+              }>
+              <svg className='arrow_right' width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white"/>
+              </svg>
+            </div>
           </div>
         )}
         {Images.length > 1 && (
